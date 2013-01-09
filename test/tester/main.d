790be9cf43f034110bdadbfed8bd983817258361
Ludovit Lucenic <llucenic@gmail.com>
2013-01-09 18:16:45 +0100

    modified test project (interface, abstract class function parameter)

--- test/tester/main.d
+++ test/tester/main.d
@@ -2,7 +2,7 @@ module tester.main;
 
 import std.stdio;
 
-float globalF = 4.5f;
+float _globalF = 4.5f;
 
 interface IAbstract
 {
@@ -11,16 +11,25 @@ interface IAbstract
 
 class Ancestor : IAbstract
 {
-	public ubyte ub;
+	public bool bo;
+	private ubyte pub;
 	
-	uint getValue(IAbstract ia) { return 0; }
+	uint getValue(IAbstract ia) { return -2; }
 }
 
 class Descendant : Ancestor
 {
 	private real r;
 	this(real ra) { this.r = ra; }
-	wchar nexMethod(string str) { return 'ä'; }
+	long nextMethod(real myParam) { r = myParam; return 154L; }
+	wchar prevMethod(string str) { return 'ä'; }
+}
+
+class NextDescendant : Descendant
+{
+	int* ip;
+	this(ref int ri) { super(-1); this.ip = &ri; }
+	long furtherMethod() { r = 7; return 87L; }
 }
 
 
@@ -28,6 +37,11 @@ void main(string[] args)
 {
 	int i = 6;
 	fun(12, "osem", new Descendant(6.8));
+
+	float f;
+	int refi = 13;
+	auto b = fun2(12, "osem", new NextDescendant(refi));
+	b++;
 }
 
 ubyte fun(byte b, immutable(char)[] ica, IAbstract ia)
@@ -38,10 +52,27 @@ ubyte fun(byte b, immutable(char)[] ica, IAbstract ia)
     // Prints "Hello World" string in console
     writeln(myString);
     
-    // Lets the user press <Return> before program returns
-    //stdin.readln();
-	globalF++;
+	_globalF++;
 	
+	return --b;
+}
+
+byte fun2(byte b, immutable(char)[] iac, Object ia)
+{
+	int i = 9;
+	ulong ul = 15UL;
+	string myString = "Hello World!";
+	
+	// Prints "Hello World" string in console
+	if (auto castia = cast(Descendant)ia) {
+		real r = castia.r;
+		r++;
+		i += r;
+		r += castia.nextMethod(58.9);
+	}
+	writeln(myString);
+    
+	_globalF++;
 	return --b;
 }
 
